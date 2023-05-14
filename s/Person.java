e05cbb5c902772326b52391197d56fb07db3845f
Taleh Farzaliyev <taleh.farzaliyev@azericard.com>
2023-05-14 22:44:42 +0400

    initial commit

---
+++ s/Person.java
@@ -0,0 +1,20 @@
+package s;
+
+public class Person {
+    private Long personId;
+    private String firstName;
+    private String lastName;
+    private String age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+}
